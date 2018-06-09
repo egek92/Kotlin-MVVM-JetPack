e1ca5bd56fa0ad6570dbc5d1004278c4334debc5
Ege <egek92@yandex.com>
2018-06-10 00:31:45 +0300

    data fatched

--- app/src/main/kotlin/com/denwehrle/boilerplate/CryptocurrencyApplication.kt
+++ app/src/main/kotlin/com/denwehrle/boilerplate/CryptocurrencyApplication.kt
@@ -3,9 +3,12 @@ package com.denwehrle.boilerplate
 import android.app.Activity
 import android.app.Application
 import com.denwehrle.boilerplate.di.module.AppModule
+import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -19,6 +22,18 @@ class CryptocurrencyApplication : Application(), HasActivityInjector {
   override fun onCreate() {
     super.onCreate()
     DaggerAppComponent.builder().appModule(AppModule(this)).build().inject(this)
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+      Stetho.initializeWithDefaults(this)
+    }
+
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      return
+    }
+    LeakCanary.install(this)
+
+
   }
 
   override fun activityInjector(): AndroidInjector<Activity> = activityInjector
--- app/src/main/kotlin/com/denwehrle/boilerplate/ui/CryptocurrenciesActivity.kt
+++ app/src/main/kotlin/com/denwehrle/boilerplate/ui/CryptocurrenciesActivity.kt
@@ -3,6 +3,15 @@ package com.denwehrle.boilerplate.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.denwehrle.boilerplate.R
+import com.denwehrle.boilerplate.data.ApiClient
+import com.denwehrle.boilerplate.data.ApiInterface
+import com.denwehrle.boilerplate.data.Cryptocurrency
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 /**
  * Created by Ege Kuzubasioglu on 9.06.2018 at 23:51.
@@ -11,8 +20,53 @@ import com.denwehrle.boilerplate.R
 
 
 class CryptocurrenciesActivity : AppCompatActivity() {
+
+  val compositeDisposable: CompositeDisposable()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_crypto_currency)
+
+
+    showCryptocurrencies()
+  }
+
+  private fun showCryptocurrencies() {
+    val cryptocurrenciesResponse = getCryptocurrencies()
+        .subscribeOn(Schedulers.newThread())
+        .observeOn(AndroidSchedulers.mainThread())
+
+    val disposableObserver =
+        cryptocurrenciesResponse.subscribeWith(object : DisposableObserver<List<Cryptocurrency>>() {
+          override fun onComplete() {
+          }
+
+          override fun onNext(cryptocurrencies: List<Cryptocurrency>) {
+            val listSize = cryptocurrencies.size
+            Timber.e("$listSize items are available")
+          }
+
+          override fun onError(e: Throwable) {
+            Timber.e(e.message)
+          }
+
+        })
+
+    compositeDisposable.addAll(disposableObserver)
+
+  }
+
+  private fun getCryptocurrencies(): Observable<List<Cryptocurrency>> {
+    val retrofit = ApiClient.getClient()
+    val apiInterface = retrofit.create(ApiInterface::class.java)
+    return apiInterface.getCryptocurrencies("0")
+  }
+
+
+  override fun onDestroy() {
+    if (!compositeDisposable.isDisposed) {
+      compositeDisposable.dispose()
+    }
+    super.onDestroy()
   }
 }
